b8abf614e84709be75095d23e07817f3bf9c4ca9
marian.sabianaa <marian.sabianaa@70280701-655c-5af6-a42b-ffe49c2aa791>
2011-04-03 16:13:57 +0000

    todavia no probe la multi y la sub... pero deberia andar, me fui a comer!

--- tp1/code/Real.h
+++ tp1/code/Real.h
@@ -16,8 +16,8 @@ class Real {
 		~Real();
 		
 		Real operator+ (const Real &a) const; //--> van a ser constantes o no? PENSAR
-		Real operator* (Real a);
-		Real operator- (Real a);
+		Real operator* (const Real &a) const;
+		Real operator- (const Real &a) const;
 //		Real operator/ (Real a, Real b);
 //		Real operator= (Real a);
 //		Real operator= (ullInt a);
