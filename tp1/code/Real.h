60b57a95503b1d232d476310c88a090ea6912cf4
marian.sabianaa <marian.sabianaa@70280701-655c-5af6-a42b-ffe49c2aa791>
2011-04-03 15:13:46 +0000

    vamos mejorando!

--- tp1/code/Real.h
+++ tp1/code/Real.h
@@ -14,17 +14,19 @@ class Real {
 		Real(llInt value, int t_digits,bool truncates);
 		~Real();
 		
-		Real operator+ (Real a); //--> van a ser constantes o no? PENSAR
+		const Real operator+ (const Real &a) const; //--> van a ser constantes o no? PENSAR
 		Real operator* (Real a);
 		Real operator- (Real a);
 //		Real operator/ (Real a, Real b);
 //		Real operator= (Real a);
 //		Real operator= (ullInt a);
+		const Real operator= (const Real &a)const;
 		Real raizCuad();
 	
 		void printReal();
 
 	private:
+		void copyDoubleToArray(double number);
 		void copyDoubleToArray(ullInt sign, ullInt exp, ullInt mantissa);
 		ullInt getSign();
 		ullInt getExp();
