c63baeccbe34e389675439838c64887e4addac54
marian.sabianaa <marian.sabianaa@70280701-655c-5af6-a42b-ffe49c2aa791>
2011-04-11 00:23:16 +0000

    qdo los tests en el main.cpp aparentemente anda, ahora yo me pregunto por el +5 en la iteracion???

--- trunk/tp1/code/main.c
+++ trunk/tp1/code/main.c
@@ -5,3 +5,47 @@ int main(){
 
 	return 0;
 }
+
+
+/----------------------		
+/*	t_digits = atoi(argv[1]);
+	truncate = (bool) atoi(argv[2]);
+	
+	Real _1(1,t_digits,truncate);	
+	Real _2(2,t_digits,truncate);	
+	Real _3(3,t_digits,truncate);
+	Real res(0,t_digits,truncate);
+
+	res = _1/_3;
+	res.printReal();
+//	cout << _1 << endl;
+
+//	res.printReal();
+/*	res = _1/_2;
+
+	cout << "RES = " << endl;
+	res.printReal();
+
+	res = res/_3;
+	
+	cout << "res/3" << endl;
+	res.printReal();
+	
+	res = res+(_1/_2)+(_1/_3);
+	
+	cout << "res/3 + 1/2 + 1/3" << endl;
+	res.printReal();
+
+	res = res/_2;
+	
+	cout << "res/4" << endl;
+	res.printReal();
+	
+	res = res/_2;
+	
+	cout << "res/8" << endl;
+	res.printReal();
+*/
+	return 0;
+
+//-------------------
