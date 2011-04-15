98cce72b3626c4b5df6d9f30b530608abe812707
marian.sabianaa <marian.sabianaa@70280701-655c-5af6-a42b-ffe49c2aa791>
2011-04-15 00:45:22 +0000

    espero q no lo haya roto todo

--- tp1/code/main.cpp
+++ tp1/code/main.cpp
@@ -26,21 +26,15 @@ int main(int argc, char** argv){
 	int  cantIt = 10;
 	bool truncate = true;
 
-/*
-	Real _1(1,atoi(argv[1]),false);
-	Real _2(2,atoi(argv[1]),false);
-	Real _3(3,atoi(argv[1]),false);
-	Real res(atoi(argv[1]),false);
-	
-	res = (_1/_2)+ (_1/_3) + _3;
-	res.printReal();
-	cout << "filtrando presicion WOWOWOWOWOW" << endl;
-	res.filterPrecision();
-	cout << "FILTRADO" << endl;
-	res.printReal();
-		
-	return 0;
-*/
+	if(argc<2){
+		usage();
+		exit(0);
+	}
+	else
+		if(!eligeModoUso(argv)){
+			checkForHelp(argv);
+			exit(0);
+		}
 
 	switch(argc){
 		case 2:
@@ -53,32 +47,29 @@ int main(int argc, char** argv){
 			cout << "\t trunca? = " << truncate << endl;
 			break;
 		case 4:
-			t_digits = atoi(argv[3]);
+			t_digits = (atoi(argv[3])<=51) ? atoi(argv[3]) : t_digits;
 			cout << "Uso de parametros por defecto: " << endl;
 			cout << "\t cantidadIteraciones = " << cantIt << endl;
 			cout << "\t trunca? = " << truncate << endl;
 			break;
 		case 5:
-			t_digits = atoi(argv[3]);
+			t_digits = (atoi(argv[3])<=51) ? atoi(argv[3]) : t_digits;
 			cantIt   = atoi(argv[4]);
 			cout << "Uso de parametros por defecto: " << endl;
 			cout << "\t trunca? = " << truncate << endl;
 			break;
 		case 6:	
-			t_digits = atoi(argv[3]);
+			t_digits = (atoi(argv[3])<=51) ? atoi(argv[3]) : t_digits;
 			cantIt   = atoi(argv[4]);
-			if (strcmp(argv[5],"-r")==0) truncate = false;
+			if(!eligeModoTruncamiento(argc,argv))
+				if (strcmp(argv[5],"-r")==0) 
+					truncate = false;
 			break;
 		default:
 			usage();
 			exit(0);
 	}
 	
-	if(t_digits > 51 || !eligeModoTruncamiento(argc,argv) || !eligeModoUso(argv)){
-		usage();
-		exit(0);
-	}
-
 	cout.precision(t_digits);
 	switch(atoi(argv[2])){
 		case 1:	
@@ -112,19 +103,16 @@ void out(SERIES* funcion, char* arg, int cantIt, int t_digits, bool truncate){
 		for(int j=1; j<=t_digits; j++){
 			cout.precision(j);
 			cout << j << "\t" << funcion(j, cantIt, truncate) << endl;
-			//funcion(j, cantIt, truncate).printRealBits();
 		}
 	}
 	else{
 		if(strcmp(arg,"-digitos")==0){
 			for(int j=1; j<=cantIt; j++){
 				cout << j << "\t" << funcion(t_digits, j, truncate) << endl;
-				//funcion(t_digits, j, truncate).printRealBits();
 			}
 		}
 		else{
 			cout << funcion(t_digits, cantIt, truncate) << endl;
-			//funcion(t_digits, cantIt, truncate).printRealBits();
 		}
 	}
 }
@@ -148,7 +136,8 @@ void checkForHelp(char** argv){
 
 void usage(){
 	cout << "uso: ./pi [modoUso] [metodo] [tdigitos] [cantidadIteraciones] [trunca?]" << endl << "(opcionales los ultimos tres parametros) " << endl << endl;
-	cout << "Para mas informacion usar ./pi --help" << endl;
+	cout << "Para mas informacion usar ./pi --help" << endl << endl;
+	cout << "El algoritmo de redondeo presenta inconsistencias, se les pide disculpas..." << endl;
 }
 
 void help(){
