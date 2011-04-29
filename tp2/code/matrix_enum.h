98666e998120818df72f9a02e2398298f5cea6db
marian.sabianaa <marian.sabianaa@70280701-655c-5af6-a42b-ffe49c2aa791>
2011-04-29 02:04:52 +0000

    lo de matrix creator no le veo mucho futuro... no se como parametrizarlo a T todavia...

---
+++ tp2/code/matrix_enum.h
@@ -0,0 +1,18 @@
+#ifndef __MATRIX_ENUM_H__
+#define __MATRIX_ENUM_H__
+
+#include "includes.h"
+
+enum MatrixType {
+	Diagonal,
+	TriangInf,
+	TriangSup,
+	Nula,
+	Identidad,
+	RandomData
+	/*
+		Alguna otra?
+	*/
+};
+
+#endif
