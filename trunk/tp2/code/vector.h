ee5c3f26dba1ca671b5d92e3611dcf05d8dfa1de
marian.sabianaa <marian.sabianaa@70280701-655c-5af6-a42b-ffe49c2aa791>
2011-04-30 18:09:43 +0000

    faltaba incluir las excepciones!

--- trunk/tp2/code/vector.h
+++ trunk/tp2/code/vector.h
@@ -1,6 +1,21 @@
 #ifndef __VECTOR_H__
 #define __VECTOR_H__
 
+#include "includes.h"
+
+template <class T>
+class Vector : public MatrixBase<T>{
+	public:
+		Vector(uInt dim);
+		~Vector();
+	
+	
+	private:
+	
+};
+
+template <typename T>
+Vector<T> :: Vector(){}
 
 
 #endif
