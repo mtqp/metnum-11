b831b850c425b2ab70f8f927fc3b6b53d5baa6ce
livornocarla <livornocarla@70280701-655c-5af6-a42b-ffe49c2aa791>
2011-05-08 17:32:42 +0000

    main funcionando

--- trunk/tp2/code/includes.h
+++ trunk/tp2/code/includes.h
@@ -14,7 +14,6 @@ using namespace std;
 #define ullInt unsigned long long
 #define llInt  long long
 #define uInt   unsigned int
-#define forn(i,n) for(int i=0;i<n;i++)
-#define form(i,n) for(int i=1;i<=n;i++)
+#define default_threshold 5
 
 #endif
--- trunk/tp2/code/structs.h
+++ trunk/tp2/code/structs.h
@@ -1,21 +1,28 @@
 #ifndef __STRUCTS_H__
 #define __STRUCTS_H__
 
+#include <utility>
 #include "includes.h"
+#include "matrix.h"
+#include "vector.h"
 
 struct warpData {
+	uInt  turn;
+	Vector<double> position;
+	double threshold;
+	bool failedAttack;
+	
+	/* Si es el primer turno quedan con ceros */
 	Matrix<double> A;
 	Vector<double> d;
-	Vector<double> position;
-	Vector<double> previous_d[];		//poner tuplas!!!!
-	uInt  turn; 
-	double threshold; 
-	bool  failedAttack;	
+	warpData(uInt dim) : A(dim), d(dim), position(dim){}
+	pair<Vector<double>,double> previous_d[];
 };
 
 struct attackData{
 	Matrix<double> A;
 	Vector<double> d;
+	attackData(uInt dim) : A(dim), d(dim){}
 };
 
 enum Strategy{
