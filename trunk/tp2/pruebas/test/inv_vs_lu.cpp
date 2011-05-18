4cd5f6028e189c313c72b72de1e15543bcbf078d
livornocarla <livornocarla@70280701-655c-5af6-a42b-ffe49c2aa791>
2011-05-18 03:12:20 +0000

    pruebas!

---
+++ trunk/tp2/pruebas/test/inv_vs_lu.cpp
@@ -0,0 +1,50 @@
+#include "../../code/includes.h"
+#include "../../code/structs.h"
+#include "../../code/matrix.h"
+#include "../../code/vector.h"
+#include "../../code/warp_cannon.h"
+#include "../../code/linear_system.h"
+
+int main(int argc, char** argv){
+	srand(time(NULL));
+	cout.precision(20);
+	cout.setf(ios::scientific,ios::floatfield);
+
+	uInt dim;
+	double err_inv;
+	double err_lu;
+	for(uInt i=2; i<=atoi(argv[1]); i++){
+		dim = i;
+		
+		/* Matriz mal condicionada */
+		Matrix<double> bad_cond(dim,BadK);
+		bool isInversible = bad_cond.isInversible(); 
+
+		/* Posicion que intenta descubrir */
+		Vector<double> pos(dim);
+		pos.createRandomVector();
+		
+		/* Punto donde pretende disparar */
+		Vector<double> d(dim);
+		d = (bad_cond*pos.traspuesta()).traspuesta();
+		isInversible = bad_cond.isInversible(); 
+		
+		linearSystem ls(bad_cond,d);
+		
+		Vector<double> inv(dim);
+		inv = ls.usingInverse();
+		
+		Vector<double> lu(dim);
+		lu = ls.usingLU();
+		
+		for(uInt j=1; j<=dim; j++){
+			err_inv += abs(inv.getValue(j) - pos.getValue(j));
+			err_lu += abs(lu.getValue(j) - pos.getValue(j));
+		}
+		
+		//cout << err_inv << " " << err_lu << endl;
+		cout << i << " " << err_inv << endl;
+	}
+	
+	return 0;
+}
