1cd14e988c15e5e6a0f719276f2b05b6188bdba7
livornocarla <livornocarla@70280701-655c-5af6-a42b-ffe49c2aa791>
2011-06-07 01:33:25 +0000

    empezando tp3

---
+++ trunk/tp3/code/spline.h
@@ -0,0 +1,16 @@
+#ifndef __SPLINE_H__
+#define __SPLINE_H__
+
+class Spline{
+	public:
+		Spline(int n, float* control);
+		~Spline();
+		
+		//funcion que modifique un punto
+		
+	private:
+	
+		//funcion que busque el punto mas cercano perteneciente a la curva
+};
+
+#endif
